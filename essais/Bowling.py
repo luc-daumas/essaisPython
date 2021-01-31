f0aed892ea0090533bb1063d3fd77a0df1d89a21
luc-daumas <luc.038@orange.fr>
2021-01-31 16:42:21 +0100

    exemple comptage de points au rugby et bowling

--- essais/Bowling.py
+++ essais/Bowling.py
@@ -8,20 +8,22 @@ def estUnStrike(tour):
     return tour[0] == 10
 
 
-def score(tour, tourPlusUn, tourPlusDeux):
+def calculLeScorePourUnTour(tour, tourPlusUn, tourPlusDeux):
     """Retourne le nombre de points marqués pour un tour
     on fait la somme des 2 lancers
     si le tour est un spare, on ajoute le premier lancer du tour suivant
     si le tour est un strike, on ajoute les 2 lancers suivants
-    >>> score([0, 0], [0, 0], [0, 0])
+    >>> calculLeScorePourUnTour([0, 0], [0, 0], [0, 0])
     0
-    >>> score([10, 0], [10, 0], [10, 0])
+    >>> calculLeScorePourUnTour([1, 2], [1, 1], [1, 1])
+    3
+    >>> calculLeScorePourUnTour([10, 0], [10, 0], [10, 0])
     30
-    >>> score([3, 7], [0, 0)], [0, 0])
+    >>> calculLeScorePourUnTour([3, 7], [0, 0], [0, 0])
     10
-    >>> score([3, 7], [4, 6], [5, 6, 7])
+    >>> calculLeScorePourUnTour([3, 7], [4, 6], [5, 6, 7])
     14
-    >>> score([5, 1], [0, 0], [10, 0])
+    >>> calculLeScorePourUnTour([5, 1], [0, 0], [10, 0])
     6
     """
     score = tour[0] + tour[1]
@@ -53,7 +55,7 @@ def avantDernierScore(tour, dernierTour):
     score = tour[0] + tour[1]
     if estUnSpare(tour):
         score += dernierTour[0]
-    if estUnStrike(dernierTour):
+    if estUnStrike(tour):
         score += dernierTour[0] + dernierTour[1]
     return score
 
@@ -61,13 +63,13 @@ def avantDernierScore(tour, dernierTour):
 def dernierScore(dernierTour):
     """Retourne le nombre de points marqués pour un tour
     on fait la somme des 3 lancers
-    >>> dernierScore(0, 0, 0)
+    >>> dernierScore([0, 0, 0])
     0
-    >>> dernierScore(10, 10, 10)
+    >>> dernierScore([10, 10, 10])
     30
-    >>> dernierScore(3, 7, 0)
+    >>> dernierScore([3, 7, 0])
     10
-    >>> dernierScore(3, 7, 4)
+    >>> dernierScore([3, 7, 4])
     14
     """
     score = dernierTour[0] + dernierTour[1] + dernierTour[2]
@@ -84,7 +86,7 @@ if __name__ == '__main__':
     scoreTour = 0
     for indiceTour in range(0, len(listeTours)):
         if indiceTour < 8:
-            scoreTour = score(listeTours[indiceTour], listeTours[indiceTour + 1], listeTours[indiceTour + 2])
+            scoreTour = calculLeScorePourUnTour(listeTours[indiceTour], listeTours[indiceTour + 1], listeTours[indiceTour + 2])
         if indiceTour == 8:
             scoreTour = avantDernierScore(listeTours[indiceTour], listeTours[indiceTour + 1])
         if indiceTour == 9:
@@ -99,7 +101,7 @@ if __name__ == '__main__':
     scoreTour = 0
     for indiceTour in range(0, len(listeTours)):
         if indiceTour < 8:
-            scoreTour = score(listeTours[indiceTour], listeTours[indiceTour + 1], listeTours[indiceTour + 2])
+            scoreTour = calculLeScorePourUnTour(listeTours[indiceTour], listeTours[indiceTour + 1], listeTours[indiceTour + 2])
         if indiceTour == 8:
             scoreTour = avantDernierScore(listeTours[indiceTour], listeTours[indiceTour + 1])
         if indiceTour == 9:
